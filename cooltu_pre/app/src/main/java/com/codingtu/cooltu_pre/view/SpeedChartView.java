9c8d73faa7675680502a70d4a0f38d6e62133529
cooltu <cooltu@codingtu.com>
2023-04-20 10:40:05 +0800

    v1.0.17 修改cooltu4j的版本

--- cooltu_pre/app/src/main/java/com/codingtu/cooltu_pre/view/SpeedChartView.java
+++ cooltu_pre/app/src/main/java/com/codingtu/cooltu_pre/view/SpeedChartView.java
@@ -1,12 +1,9 @@
 package com.codingtu.cooltu_pre.view;
 
 import android.content.Context;
-import android.graphics.Paint;
 import android.util.AttributeSet;
 
 import com.codingtu.cooltu.lib4a.view.chartview.CoreChartView;
-
-import cooltu.lib4j.data.bean.MaxMin;
 
 public class SpeedChartView<T> extends CoreChartView {
 
