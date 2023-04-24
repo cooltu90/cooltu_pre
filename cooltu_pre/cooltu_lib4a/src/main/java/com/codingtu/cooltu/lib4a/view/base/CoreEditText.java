594ae040614d54f3a4631a59593ceb9466b61cd6
cooltu <cooltu@codingtu.com>
2023-04-24 13:54:14 +0800

    v1.0.25      * + 增加圆角EditText

---
+++ cooltu_pre/cooltu_lib4a/src/main/java/com/codingtu/cooltu/lib4a/view/base/CoreEditText.java
@@ -0,0 +1,57 @@
+package com.codingtu.cooltu.lib4a.view.base;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.codingtu.cooltu.lib4a.R;
+import com.codingtu.cooltu.lib4a.act.OnDestroy;
+import com.codingtu.cooltu.lib4a.tools.DestoryTool;
+import com.codingtu.cooltu.lib4a.view.tools.RoundBgTool;
+
+public class CoreEditText extends androidx.appcompat.widget.AppCompatEditText implements OnDestroy {
+    private RoundBgTool roundBgTool;
+
+    public CoreEditText(@NonNull Context context) {
+        super(context);
+        init(context, null, 0);
+    }
+
+    public CoreEditText(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs, 0);
+    }
+
+    public CoreEditText(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs, defStyleAttr);
+    }
+
+    protected void init(Context context, AttributeSet attrs, int defStyleAttr) {
+        DestoryTool.onDestory(context, this);
+        roundBgTool = new RoundBgTool();
+        roundBgTool.init(context, this, attrs,
+                R.styleable.CoreEditText,
+                R.styleable.CoreEditText_android_radius,
+                R.styleable.CoreEditText_android_topLeftRadius,
+                R.styleable.CoreEditText_android_topRightRadius,
+                R.styleable.CoreEditText_android_bottomLeftRadius,
+                R.styleable.CoreEditText_android_bottomRightRadius,
+                R.styleable.CoreEditText_strokeWidth,
+                R.styleable.CoreEditText_strokeColor);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+        roundBgTool.initBackground();
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
