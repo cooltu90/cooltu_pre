0b48f23361f184526de564e23598cef31519d76b
cooltu <cooltu@codingtu.com>
2023-05-13 01:24:53 +0800

    删除CutFileImage

--- cooltu_pre/app/src/main/java/com/codingtu/cooltu_pre/ui/ViewActivity.java
+++ cooltu_pre/app/src/main/java/com/codingtu/cooltu_pre/ui/ViewActivity.java
@@ -1,35 +1,19 @@
 package com.codingtu.cooltu_pre.ui;
 
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.graphics.Rect;
-import android.graphics.RectF;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 
-import com.codingtu.cooltu.lib4a.bean.WH;
-import com.codingtu.cooltu.lib4a.image.BitmapWH;
-import com.codingtu.cooltu.lib4a.image.CutFileImage;
 import com.codingtu.cooltu.lib4a.image.FileBitmap;
-import com.codingtu.cooltu.lib4a.tools.BitmapTool;
-import com.codingtu.cooltu.lib4a.tools.DrawTool;
 import com.codingtu.cooltu.lib4a.tools.SDCardTool;
 import com.codingtu.cooltu.lib4a.tools.ViewTool;
-import com.codingtu.cooltu.processor.lib.log.Logs;
+import com.codingtu.cooltu.processor.annotation.tools.To;
+import com.codingtu.cooltu.processor.annotation.ui.ActBase;
 import com.codingtu.cooltu_pre.R;
 
 import core.actbase.ViewActivityBase;
-
-import com.codingtu.cooltu.processor.annotation.tools.To;
-import com.codingtu.cooltu.processor.annotation.ui.ActBase;
-
-import java.io.File;
-import java.io.IOException;
-
 import core.actres.ViewActivityRes;
 
 @To(ViewActivityRes.class)
