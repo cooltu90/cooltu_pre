ef0cfbe9b9bba5484153e628f59d18f0a83fec97
cooltu <cooltu@codingtu.com>
2023-11-06 10:52:39 +0800

    v1.1.33      * +++download 修改

--- cooltu_pre/cooltu_lib4a/src/main/java/com/codingtu/cooltu/lib4a/download/Download.java
+++ cooltu_pre/cooltu_lib4a/src/main/java/com/codingtu/cooltu/lib4a/download/Download.java
@@ -1,76 +1,30 @@
 package com.codingtu.cooltu.lib4a.download;
 
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
-
-import androidx.annotation.NonNull;
-
-import com.codingtu.cooltu.constant.DownloadStatus;
-import com.codingtu.cooltu.lib4a.net.NetTool;
-import com.codingtu.cooltu.lib4a.net.api.CreateApi;
-import com.codingtu.cooltu.lib4a.net.bean.CoreSendParams;
-import com.codingtu.cooltu.lib4a.net.netback.NetBackI;
-
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.List;
-
+import com.codingtu.cooltu.lib4a.net.interceptor.HeaderInterceptor;
 import com.codingtu.cooltu.lib4j.tools.StringTool;
-import io.reactivex.Flowable;
-import okhttp3.ResponseBody;
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava2.Result;
+import com.lzy.okgo.OkGo;
+import com.lzy.okgo.callback.FileCallback;
+import com.lzy.okgo.model.Progress;
+import com.lzy.okgo.model.Response;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 public class Download {
 
-    private String baseUrl;
     private String url;
     private File file;
     private File dir;
     private String fileName;
+    private String tag;
     private DownloadCallBack callBack;
-
-    public Handler handler = new Handler(Looper.getMainLooper()) {
-        @Override
-        public void handleMessage(@NonNull Message msg) {
-            super.handleMessage(msg);
-
-            switch (msg.what) {
-                case DownloadStatus.START:
-                    callBack.onStart();
-                    break;
-                case DownloadStatus.FAIL:
-                    callBack.onFail((Throwable) msg.obj);
-                    break;
-                case DownloadStatus.FINISH:
-                    callBack.onFinish((String) msg.obj);
-                    break;
-                case DownloadStatus.PROGRESS:
-                    long[] ls = (long[]) msg.obj;
-                    callBack.onProgress(ls[0], ls[1]);
-                    break;
-            }
-        }
-    };
-
+    private Long timeout;
+    private HeaderInterceptor headerInterceptor;
 
     public static Download url(String url) {
         Download download = new Download();
-        int i = url.indexOf("/", url.indexOf(":") + 3);
-        download.baseUrl = url.substring(0, i + 1);
-        download.url = url.substring(i + 1);
-        return download;
-    }
-
-    public static Download url(String baseUrl, String url) {
-        Download download = new Download();
-        download.baseUrl = baseUrl;
         download.url = url;
+        download.tag = "download";
         return download;
     }
 
@@ -98,13 +52,29 @@ public class Download {
         return this;
     }
 
+    public Download setTag(String tag) {
+        this.tag = tag;
+        return this;
+    }
+
+    public Download setTimeout(Long timeout) {
+        this.timeout = timeout;
+        return this;
+    }
+
+    public Download setHeaderInterceptor(HeaderInterceptor headerInterceptor) {
+        this.headerInterceptor = headerInterceptor;
+        return this;
+    }
+
     public Download callBack(DownloadCallBack callBack) {
         this.callBack = callBack;
         return this;
     }
 
+
     public void start() {
-        if (StringTool.isBlank(baseUrl) || StringTool.isBlank(url)) {
+        if (StringTool.isBlank(url)) {
             return;
         }
 
@@ -116,93 +86,59 @@ public class Download {
                 fileName = url.substring(url.lastIndexOf("/") + 1);
             }
             file = new File(dir, fileName);
+        } else {
+            dir = file.getParentFile();
+            fileName = file.getName();
         }
 
-        NetTool.api(new CreateApi() {
+
+        FileCallback fileCallback = new FileCallback(dir.getAbsolutePath(), fileName) {
             @Override
-            public Flowable<Result<ResponseBody>> create(Retrofit retrofit, CoreSendParams params) {
-                return retrofit.create(DownloadService.class).download(url);
+            public void onSuccess(Response<File> response) {
+                callBack.onFinish(file.getAbsolutePath());
             }
-        }, "downloadBack", baseUrl, null).io(new NetBackI() {
+
             @Override
-            public void accept(String code, Result<ResponseBody> result, CoreSendParams params, List objs) {
-                if (result.isError()) {
-                    handler.sendMessage(obtainOnFailMessage(result.error()));
-                } else {
-                    writeFileFromIS(file, result.response().body().byteStream(), result.response().body().contentLength());
-                }
+            public void onError(Response<File> response) {
+                super.onError(response);
+                callBack.onFail(response.getException());
             }
-        });
 
-    }
-
-    private static int sBufferSize = 8192;
-
-    private void writeFileFromIS(File file, InputStream is, long totalLength) {
-        //开始下载
-        handler.sendEmptyMessage(DownloadStatus.START);
-
-        //创建文件
-        if (!file.exists()) {
-            if (!file.getParentFile().exists())
-                file.getParentFile().mkdir();
-            try {
-                file.createNewFile();
-            } catch (IOException e) {
-                handler.sendMessage(obtainOnFailMessage(e));
+            @Override
+            public void downloadProgress(Progress progress) {
+                super.downloadProgress(progress);
+                callBack.onProgress(progress.totalSize, progress.currentSize);
             }
+        };
+        callBack.onStart();
+
+        OkGo okGo = OkGo.getInstance();
+        if (timeout != null && headerInterceptor != null) {
+            okGo.setOkHttpClient(
+                    okGo.getOkHttpClient()
+                            .newBuilder()
+                            .connectTimeout(timeout, TimeUnit.MILLISECONDS)
+                            .addInterceptor(headerInterceptor)
+                            .build()
+            );
+        } else if (timeout != null) {
+            okGo.setOkHttpClient(
+                    okGo.getOkHttpClient()
+                            .newBuilder()
+                            .connectTimeout(timeout, TimeUnit.MILLISECONDS)
+                            .build()
+            );
+        } else if (headerInterceptor != null) {
+            okGo.setOkHttpClient(
+                    okGo.getOkHttpClient()
+                            .newBuilder()
+                            .addInterceptor(headerInterceptor)
+                            .build()
+            );
         }
-
-        OutputStream os = null;
-        long currentLength = 0;
-        try {
-            os = new BufferedOutputStream(new FileOutputStream(file));
-            byte data[] = new byte[sBufferSize];
-            int len;
-            while ((len = is.read(data, 0, sBufferSize)) != -1) {
-                os.write(data, 0, len);
-                currentLength += len;
-                //计算当前下载进度
-                handler.sendMessage(obtainOnProgressMessage(totalLength, currentLength));
-            }
-            //下载完成，并返回保存的文件路径
-            handler.sendMessage(obtainOnFinishMessage(file.getAbsolutePath()));
-        } catch (IOException e) {
-            handler.sendMessage(obtainOnFailMessage(e));
-        } finally {
-            try {
-                is.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            try {
-                if (os != null) {
-                    os.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private Message obtainMessage(int what, Object obj) {
-        Message message = Message.obtain();
-        message.what = what;
-        message.obj = obj;
-        return message;
-    }
-
-    private Message obtainOnFailMessage(Throwable error) {
-        return obtainMessage(DownloadStatus.FAIL, error);
-    }
-
-    private Message obtainOnFinishMessage(String path) {
-        return obtainMessage(DownloadStatus.FINISH, path);
-    }
-
-    private Message obtainOnProgressMessage(long totalLen, long currentLen) {
-        long[] longs = {totalLen, currentLen};
-        return obtainMessage(DownloadStatus.PROGRESS, longs);
+        okGo.<File>get(url)
+                .tag(tag)
+                .execute(fileCallback);
     }
 
 }
---
+++ cooltu_pre/cooltu_lib4j/src/main/java/com/codingtu/cooltu/lib4j/data/bean/Progress.java
@@ -0,0 +1,6 @@
+package com.codingtu.cooltu.lib4j.data.bean;
+
+public class Progress {
+    public long totalLen;
+    public long currentLen;
+}
