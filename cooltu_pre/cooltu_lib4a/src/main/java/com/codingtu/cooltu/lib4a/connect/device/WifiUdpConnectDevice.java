e7040b76856827449966494428f7806830c26a6b
cooltu <cooltu@codingtu.com>
2023-12-18 10:12:56 +0800

    添加wifiudp连接

---
+++ cooltu_pre/cooltu_lib4a/src/main/java/com/codingtu/cooltu/lib4a/connect/device/WifiUdpConnectDevice.java
@@ -0,0 +1,23 @@
+package com.codingtu.cooltu.lib4a.connect.device;
+
+import com.codingtu.cooltu.lib4a.connect.ConnectDeviceBaseData;
+import com.codingtu.cooltu.lib4a.connect.ResponseData;
+
+public abstract class WifiUdpConnectDevice extends ConnectDevice {
+    public WifiUdpConnectDevice(int connectType, int deviceType, String name, String mac) {
+        super(connectType, deviceType, name, mac);
+    }
+
+    public WifiUdpConnectDevice(ConnectDeviceBaseData baseData) {
+        super(baseData);
+    }
+
+    @Override
+    public void connect() {
+
+
+        
+
+    }
+
+}
